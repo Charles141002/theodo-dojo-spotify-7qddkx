b774b796ebe73c313bbca5dae8e2d06e581d5231
Charles141002 <127297497+Charles141002@users.noreply.github.com>
2023-04-18 21:38:36 +0200

    Fin 1ere session

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,13 @@ import { useState } from 'react';
 import { fetchTracks } from './lib/fetchTracks';
 import { useQuery } from '@tanstack/react-query';
 
-const trackUrls = [
-  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
-  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
-  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
-  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
-  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
-];
-
 const App = () => {
-  const { data: tracks } = useQuery({
+  const { data: tracks, isLoading } = useQuery({
     queryKey: ['tracks'],
     queryFn: fetchTracks,
   });
 
+  console.log(tracks?.[0]?.track.preview_url);
   console.log(tracks?.[0]?.track.name);
   console.log(typeof tracks);
 
@@ -26,6 +19,12 @@ const App = () => {
   const goToNextTrack = () => {
     setTrackIndex(trackIndex + 1);
   };
+
+  const AlbumCover = () => {
+    const src = 'https://example.com/image.png'; // A changer ;)
+    return <img src={src} style={{ width: 400, height: 400 }} />;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -33,8 +32,14 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur mon blind test</h1>
       </header>
       <div className="App-images">
+        <div>{isLoading ? <p>Loading</p> : <p></p>}</div>
         <div>
-          <audio src={trackUrls[trackIndex]} autoPlay controls />
+          {tracks?.[trackIndex]?.track.name}
+          <audio
+            src={tracks?.[trackIndex]?.track.preview_url}
+            autoPlay
+            controls
+          />
           <button onClick={goToNextTrack}>Next track</button>
         </div>
       </div>
